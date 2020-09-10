e4f7f1c9a17c2f82e9290c3d7bb7c12ec6afdd81
seunkim <seunkim@ea.com>
2020-09-10 23:39:43 +0900

    Updated metric-report-demo2

--- js/chart.js
+++ js/chart.js
@@ -1,17 +1,74 @@
-var ctx = document.getElementById("cpu").getContext('2d');
-var cpuChart = new Chart(ctx, getChartConfig(makeDataSets(cpuData), ""));
+var element = document.getElementById("cpu")
+if (element != null) {
+    new Chart(element.getContext('2d'), getChartConfig(makeDataSets(cpuData), ""));
+}
 
-var ctx = document.getElementById("memory").getContext('2d');
-var memChart = new Chart(ctx, getChartConfig(makeDataSets(memoryData), "GB"));
+var element = document.getElementById("memory")
+if (element != null) {
+    new Chart(element.getContext('2d'), getChartConfig(makeDataSets(memoryData), "GB"));
+}
 
-var ctx = document.getElementById("network_in_byte").getContext('2d');
-var networkInChart = new Chart(ctx, getChartConfig(makeDataSets(networkInByteData), "KB"));
+var element = document.getElementById("network_in_byte")
+if (element != null) {
+    new Chart(element.getContext('2d'), getChartConfig(makeDataSets(networkInByteData), "KB"));
+}
 
-var ctx = document.getElementById("network_out_byte").getContext('2d');
-var networkOutChart = new Chart(ctx, getChartConfig(makeDataSets(networkOutByteData), "KB"));
+var element = document.getElementById("network_out_byte")
+if (element != null) {
+    new Chart(element.getContext('2d'), getChartConfig(makeDataSets(networkOutByteData), "KB"));
+}
 
-var ctx = document.getElementById("network_in_packet").getContext('2d');
-var networkInChart = new Chart(ctx, getChartConfig(makeDataSets(networkInPacketData), ""));
+var element = document.getElementById("network_in_packet")
+if (element != null) {
+    new Chart(element.getContext('2d'), getChartConfig(makeDataSets(networkInPacketData), ""));
+}
 
-var ctx = document.getElementById("network_out_packet").getContext('2d');
-var networkOutChart = new Chart(ctx, getChartConfig(makeDataSets(networkOutPacketData), ""));
+var element = document.getElementById("network_out_packet")
+if (element != null) {
+    new Chart(element.getContext('2d'), getChartConfig(makeDataSets(networkOutPacketData), ""));
+}
+
+var element = document.getElementById("gc_count_sum")
+if (element != null) {
+    new Chart(element.getContext('2d'), getChartConfig(makeDataSets(gcCountSumData), ""));
+}
+
+var element = document.getElementById("gc_count_avg")
+if (element != null) {
+    new Chart(element.getContext('2d'), getChartConfig(makeDataSets(gcCountAvgData), ""));
+}
+
+var element = document.getElementById("gc_time_spent_sum")
+if (element != null) {
+    new Chart(element.getContext('2d'), getChartConfig(makeDataSets(gcTimeSpentSumData), ""));
+}
+
+var element = document.getElementById("gc_time_spent_avg")
+if (element != null) {
+    new Chart(element.getContext('2d'), getChartConfig(makeDataSets(gcTimeSpentAvgData), ""));
+}
+
+var element = document.getElementById("heap_max")
+if (element != null) {
+    new Chart(element.getContext('2d'), getChartConfig(makeDataSets(heapMaxData), "MB"));
+}
+
+var element = document.getElementById("heap_used")
+if (element != null) {
+    new Chart(element.getContext('2d'), getChartConfig(makeDataSets(heapUsedData), "MB"));
+}
+
+var element = document.getElementById("non_heap_max")
+if (element != null) {
+    new Chart(element.getContext('2d'), getChartConfig(makeDataSets(nonHeapMaxData), "MB"));
+}
+
+var element = document.getElementById("non_heap_used")
+if (element != null) {
+    new Chart(element.getContext('2d'), getChartConfig(makeDataSets(nonHeapUsedData), "MB"));
+}
+
+var element = document.getElementById("threads")
+if (element != null) {
+    new Chart(element.getContext('2d'), getChartConfig(makeDataSets(threadsData), ""));
+}
--- js/datatables.js
+++ js/datatables.js
@@ -28,45 +28,10 @@ $(document).ready(function() {
   });
 });
 
-
-// Call the dataTables jQuery plugin
-$(document).ready(function() {
-  $('#serverProfile').DataTable({
-    data: serverProfile,
-    columns: [
-      { data: 'serviceName' },
-      { data: 'version' },
-      { data: 'podCount' },
-      { data: 'serviceType' },
-      { data: 'createdAt' }
-    ]
-  });
-});
-
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
   $('#serverMetric').DataTable({
     pageLength: 100,
-    'rowCallback': function(row, data, index) {
-      console.log(data.cpu)
-      console.log(data.memory)
-      var reg = /([0-9.]*)% \/ ([0-9.]*)% \/ ([0-9.]*)%/i;
-      var cpuMatchs = data.cpu.match(reg);
-
-      var isCpuOverTarget = false;
-
-      var cpuCriteria = parseFloat(data.cpuCriteria.replace('%', ''));
-
-      for (i=1; i < 4; i++) {
-        if (parseFloat(cpuMatchs[i]) >= cpuCriteria) {
-          isCpuOverTarget = true;
-        }
-      }
-
-      if (isCpuOverTarget) {
-        $('td:eq(2)', row).css('color', 'red');
-      }
-    },
     data: serverMetric,
     columns: [
       { data: 'serviceName' },
